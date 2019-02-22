f4d07f41e03ad9c7f3a4ef080be250c37b94ca9d
Dragomir Yanakiev <dragomir.yanakiev@tick42.com>
2019-02-22 14:51:07 +0200

    styling changes/renaming

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import Converter from '../converter/index'
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import Historic from '../historic-data/index'
 import HistoricAll from '../historic-data-all/index'
-import store from '../../store/index'
 import './App.css';
 
 class App extends Component {
